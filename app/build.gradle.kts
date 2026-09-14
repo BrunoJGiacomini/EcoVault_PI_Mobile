4e8ede528c2994c54b8a3ef1308b29a90f3fe914
BrunoJGiacomini <brunojunigiacomini@gmail.com>
2026-09-14 18:15:32 -0300

    chore(deps): adiciona dependências do Room ao build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.material)
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("androidx.room:room-runtime:2.6.1")
+    annotationProcessor ("androidx.room:room-compiler:2.6.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
